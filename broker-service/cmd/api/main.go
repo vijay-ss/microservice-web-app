fbd509fcae700d71933680aea4d408b62f960585
vijay-ss <54510959+vijay-ss@users.noreply.github.com>
2024-03-08 22:58:54 -0500

    add front end service (#1)
    * configure front end and broker service
    
    * update gitignore
    
    ---------
    
    Co-authored-by: Vijay Saddi <vijaysaddi@Vijays-MacBook-Pro.local>

---
+++ broker-service/cmd/api/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
+
+const webPort = "80"
+
+type Config struct {
+	Rabbit         *amqp.Connection
+}
+
+func main() {
+	// app := Config{}
+
+	log.Printf("Starting broker service on port %s\n", webPort)
+
+	srv := &http.Server{
+		Addr: fmt.Sprintf(":%s", webPort),
+		// Handler: app.routes(),
+	}
+
+	err := srv.ListenAndServe()
+	if err != nil {
+		log.Panic(err)
+	}
+
+}
