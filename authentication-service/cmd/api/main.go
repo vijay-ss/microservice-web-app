0f0d522536f7cac39e36d7617cda91587892aa0d
Vijay Saddi <vijaysaddi@Vijays-MacBook-Pro.local>
2024-06-16 19:47:41 -0400

    add tests for authenticate at mock logger api

--- authentication-service/cmd/api/main.go
+++ authentication-service/cmd/api/main.go
@@ -18,8 +18,8 @@ const webPort = "80"
 var counts int64
 
 type Config struct {
-	DB     *sql.DB
-	Models data.Models
+	Repo data.Repository
+	Client *http.Client
 }
 
 func main() {
@@ -32,8 +32,7 @@ func main() {
 	}
 
 	app := Config{
-		DB:     conn,
-		Models: data.New(conn),
+		Client: &http.Client{},
 	}
 
 	srv := &http.Server{
@@ -85,4 +84,9 @@ func openDB(dsn string) (*sql.DB, error) {
 	}
 
 	return db, nil
+}
+
+func (app *Config) setupRepo(conn *sql.DB) {
+	db := data.NewPostgresRepository(conn)
+	app.Repo = db
 }
